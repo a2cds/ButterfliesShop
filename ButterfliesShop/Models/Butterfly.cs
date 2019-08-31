5107473daee99082bc9c6207e0af08b7f880fae0
Antonio Carlos da Silva <acdasilva@gmail.com>
2019-08-30 22:25:46 -0300

    Adicionado Validation

--- ButterfliesShop/Models/Butterfly.cs
+++ ButterfliesShop/Models/Butterfly.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Http;
 using System.ComponentModel.DataAnnotations;
+using ButterfliesShop.Validators;
 
 namespace ButterfliesShop.Models
 {
@@ -12,18 +13,25 @@ namespace ButterfliesShop.Models
     {
         public int Id { get; set; }
         [Display(Name = "Common Name:")]
+        [Required(ErrorMessage = "Please enter the butterfly name")]
         public string CommonName { get; set; }
         [Display(Name = "Butterfly Family:")]
+        [Required(ErrorMessage = "Please select the butterfly family")]
         public Family? ButterflyFamily { get; set; }
         [Display(Name = "Butterflies Quantity:")]
+        [Required(ErrorMessage = "Please select the butterfly quantity")]
+        [MaxButterflyQuantityValidation(50)]
         public int? Quantity { get; set; }
         [Display(Name = "Characteristics:")]
+        [Required(ErrorMessage = "Please type the characteristics")]
+        [StringLength(50)]
         public string Characteristics { get; set; }
         [DataType(DataType.DateTime)]
         [Display(Name = "Updated on:")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yy}")]
         public DateTime CreatedDate { get; set; }
         [Display(Name = "Butterflies Picture:")]
+        [Required(ErrorMessage = "Please select the butterflies picture")]
         public IFormFile PhotoAvatar { get; set; }
         public string ImageName { get; set; }
         public byte[] PhotoFile { get; set; }
